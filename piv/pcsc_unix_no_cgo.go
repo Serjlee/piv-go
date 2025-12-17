06da07994a90963a0350ea5d81f21a6ce8467d55
Luca Osti <luca.d.osti@gmail.com>
2025-12-17 14:43:45 +0100

    Always skip cgo in linux

--- piv/pcsc_unix_no_cgo.go
+++ piv/pcsc_unix_no_cgo.go
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build (!cgo || pcscgo) && (linux || freebsd)
-// +build !cgo pcscgo
+//go:build linux || freebsd
 // +build linux freebsd
 
 package piv
